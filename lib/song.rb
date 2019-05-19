af56bfc323c618281ef6507245b8ad8b91fdb7d5
Janice Pena <jipena94@gmail.com>
2019-05-19 00:48:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     self.all.find{|person| person.name == name}
   end
   
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
     if self.find_by_name(name) == nil
       self.create_by_name(name)
       else
