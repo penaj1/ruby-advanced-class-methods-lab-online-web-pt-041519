1dd420e9edec055f15354dbbbd671ae02e0b4315
Janice Pena <jipena94@gmail.com>
2019-05-19 00:44:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   end
   
   def self.find_or_create_by_name
-    self.find_by_name(name) && self.create_by_name(new_song_name)
+    self.find_by_name(name) || self.create_by_name(new_song_name)
   end 
   
   def self.alphabetical
