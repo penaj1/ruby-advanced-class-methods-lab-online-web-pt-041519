77ea9c4cf7f3b325f5c63ee6968eee57b1ae9995
Janice Pena <jipena94@gmail.com>
2019-05-19 00:52:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,13 @@ class Song
   end 
   
   def self.create_from_filename(filename)
+    new_file = filename.split(" - ")
+    artist_name = new_file[0]
+    song_name = new_file[1].gsub(".mp3","")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
   end 
   
   
